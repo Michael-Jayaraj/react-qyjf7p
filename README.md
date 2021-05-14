fbaa25bde7ef58a4f5106bdd73749be599c7a5cf
Michael-Jayaraj <michaeljayaraj1@gmail.com>
2021-05-14 23:14:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-qyjf7p
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-qyjf7p)
---
+++ src/App1.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App1 extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+    <p>Michael</p>
+    );
+  }
+}
+
+export default App1;
